4ac2aa1b3e35743a0164971ac66d84148b57bc63
jacobhsu <jacob.hsu.tw@gmail.com>
2020-03-20 11:14:05 +0800

    remove aws serverless

--- node_scripts/deploy_api.js
+++
@@ -1,8 +0,0 @@
-const { argParse, run } = require("./helpers");
-
-async function deployAPI() {
-  const args = argParse();
-  await run("./node_modules/.bin/sls", [ "deploy", "-v", "-s", args.env ]);
-}
-
-deployAPI();
